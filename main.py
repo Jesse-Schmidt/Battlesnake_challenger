87b9005ac39d2e140d303b4bc8ae785dffb82b8a
Jesse Schmidt <schmidt.j.jesse@gmail.com>
2022-11-17 17:51:17 -0700

    add logic to stay on the board

--- main.py
+++ main.py
@@ -67,11 +67,20 @@ def move(game_state: typing.Dict) -> typing.Dict:
     elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
         is_move_safe["up"] = False
 
+    #avoid hitting the walls
     board_width = game_state['board']['width']
     board_height = game_state['board']['height']
+    if my_head["x"] == 0:
+        is_move_safe["left"] = False
+    elif my_head["x"] == board_width-1:
+        is_move_safe['right'] = False
+    if my_head["y"] == 0:
+        is_move_safe["down"] = False
+    elif my_head["y"] == board_width-1:
+        is_move_safe['up'] = False
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
+    my_body = game_state['you']['body']
 
     # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     # opponents = game_state['board']['snakes']
