dd84243c39ed7cc47f6aedb6e39210077eb17249
Jesse Schmidt <schmidt.j.jesse@gmail.com>
2022-11-17 19:52:08 -0700

    add competitive logic

--- logic.py
+++ logic.py
@@ -1,67 +1,109 @@
-
+import math
 import random
 import typing
 
-def check_my_body(is_move_safe, game_state):
+def check_my_body(body_move_safe, directional_adjustments, game_state):
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
     my_body = game_state['you']['body']
-
-    for body_part in my_body:
-        if body_part['x'] + 1 == my_head['x']:
-            is_move_safe['left'] = False
-        elif body_part['x'] - 1 == my_head['x']:
-            is_move_safe['right'] = False
-        elif body_part['y'] + 1 == my_head['y']:
-            is_move_safe['down'] = False
-        elif body_part['y'] - 1 == my_head['y']:
-            is_move_safe['up'] = False
-    return is_move_safe
-
-def check_environment(is_move_safe, game_state):
-    my_head = game_state["you"]["body"][0]  # Coordinates of your head
-    #avoid hitting the walls
     board_width = game_state['board']['width']
     board_height = game_state['board']['height']
-    if my_head["x"] == 0:
-        is_move_safe["left"] = False
-    elif my_head["x"] == board_width-1:
-        is_move_safe['right'] = False
-    if my_head["y"] == 0:
-        is_move_safe["down"] = False
-    elif my_head["y"] == board_height-1:
-        is_move_safe['up'] = False
-    return is_move_safe
+
+    for direction in body_move_safe:
+        if body_move_safe[direction]:
+            temp_move = {'x': my_head['x'] + directional_adjustments[direction][0], 'y': my_head['y'] + directional_adjustments[direction][1]}
+            edge_check = temp_move['x'] < 0 or temp_move['y'] < 0 or temp_move['x'] > board_width -1 or temp_move['y'] > board_height -1
+            if temp_move in my_body or ("wrapped" not in game_state['game']['ruleset']['name'] and edge_check):
+                body_move_safe[direction] = 0
+    
+    return body_move_safe
+
+def check_environment(envi_move_safe, directional_adjustments, game_state):
+    opponents = game_state['board']['snakes']
+    hazards = game_state['board']['hazards']
+    my_head = game_state["you"]["body"][0]
+    dangerous_locations = []
+    for snake in opponents:
+        for body_part in snake['body']:
+            if body_part not in dangerous_locations:
+                dangerous_locations.append(body_part)
+    for hazard in hazards:
+        if hazard not in dangerous_locations:
+            dangerous_locations.append(hazard)
+
+    for direction in envi_move_safe:
+        if envi_move_safe[direction]:
+            temp_move = {'x': my_head['x'] + directional_adjustments[direction][0], 'y': my_head['y'] + directional_adjustments[direction][1]}
+            if temp_move in dangerous_locations:
+                envi_move_safe[direction] = 0
+
+    return envi_move_safe
+
+def weight_food_moves(food_move_safe, directional_adjustments, game_state):
+    food = game_state['board']['food']
+    if len(food) > 0:
+        my_head = game_state["you"]["body"][0]
+        closest_food = determine_closest_food(food, my_head)
+        x_distance = abs(closest_food['x'] - my_head['x'])
+        y_distance = abs(closest_food['y'] - my_head['y'])
+
+        for direction in food_move_safe:
+            if food_move_safe[direction]:
+                temp_move = {'x': my_head['x'] + directional_adjustments[direction][0], 'y': my_head['y'] + directional_adjustments[direction][1]}
+                temp_x_distance = abs(closest_food['x'] - temp_move['x'])
+                temp_y_distance = abs(closest_food['y'] - temp_move['y'])
+
+                if temp_x_distance <= x_distance and temp_y_distance <= y_distance:
+                    food_move_safe[direction] += 0.5
+
+    return food_move_safe
+
+def determine_closest_food(food_array, head):
+    closest_food = food_array[0]
+    x_distance = abs(closest_food['x'] - head['x'])
+    y_distance = abs(closest_food['y'] - head['y'])
+
+    for food in food_array:
+        temp_x_distance = abs(food['x'] - head['x'])
+        temp_y_distance = abs(food['y'] - head['y'])
+        if temp_x_distance < x_distance and temp_y_distance < y_distance:
+            closest_food = food
+            y_distance = temp_y_distance
+            x_distance = temp_x_distance
+    return closest_food
+
 
 def move(game_state: typing.Dict) -> typing.Dict:
-
+    print(game_state)
     is_move_safe = {
-      "up": True, 
-      "down": True, 
-      "left": True, 
-      "right": True
+      "up": 1, 
+      "down": 1, 
+      "left": 1, 
+      "right": 1
     }
-    is_move_safe = check_my_body(is_move_safe, game_state)
-    is_move_safe = check_environment(is_move_safe, game_state)
+    direction_adjustments = {'left': (-1,0), 'right': (1,0), 'up': (0,1), 'down': (0,-1)}
 
+    is_move_safe = check_my_body(is_move_safe, direction_adjustments, game_state)
+    is_move_safe = check_environment(is_move_safe, direction_adjustments, game_state)
+    is_move_safe = weight_food_moves(is_move_safe, direction_adjustments, game_state)
 
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
+    print(is_move_safe)
 
     # Are there any safe moves left?
     safe_moves = []
     for move, isSafe in is_move_safe.items():
-        if isSafe:
+        if isSafe != 0:
             safe_moves.append(move)
 
     if len(safe_moves) == 0:
         print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
         return {"move": "down"}
 
-    # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
-
-    # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    food = game_state['board']['food']
+    next_move = 'down'
+    move_weight = 0
+    for move, weight in is_move_safe.items():
+        if weight > move_weight:
+            next_move = move
+            move_weight = weight
 
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
